31c57bfac3b718fb9cb844035b3ca68527235c07
douglasBarbossa <46582903+douglasBarbossa@users.noreply.github.com>
2019-01-27 00:25:11 -0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,20 +16,40 @@ namespace SistemaLMS
             joazinho.setNome("João Ribeiro");
             joazinho.setIdade(14);
             joazinho.Matricula = "ra180";
-            joazinho.estaPresente(false);
+            joazinho.Presente = false;
+            joazinho.ResponderPresenca();
             joazinho.imprimir();
 
             Aluno ritinha = new Aluno();
             ritinha.setNome("Rita de Cassia");
             ritinha.setIdade(12);
             ritinha.Matricula = "ra181";
-            ritinha.estaPresente(true);
+            ritinha.Presente = true;
+            ritinha.ResponderPresenca();
             ritinha.imprimir();
 
             Professor jose = new Professor();
             jose.nome = "José Firmino";
-            jose.idade = 45;
+            jose.setIdade(40);
+            jose.ValorHora = 30;
+            jose.Tipo = 1;
+            jose.Bonus = 0;
+            WriteLine("Salario {0:c}", jose.CalcularSalarioFinal(120));
             jose.imprimir();
+            jose.addAluno(joazinho);
+            jose.addAluno(ritinha);
+            jose.ImprimirAlunos();
+
+            Professor maria = new Professor();
+            maria.nome = "Maria Oliveira";
+            maria.setIdade(42);
+            maria.ValorHora = 30;
+            maria.Tipo = 2;
+            maria.Bonus = 15;
+            WriteLine("Salario {0:c}", maria.CalcularSalarioFinal(120));
+            maria.imprimir();
+            maria.addAluno(joazinho);
+            maria.ImprimirAlunos();
 
             ReadLine();
         }
